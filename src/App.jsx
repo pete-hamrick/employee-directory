fe73fc2599f66089b168f8d2e91a6782d688c2b8
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-05 15:44:07 -0800

    added router, switch and route to app. Adding basic components next

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './views/Home';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/profile/edit"></Route>
+          <Route path="/profile"></Route>
+          <Route path="/login"></Route>
+          <Route path="/signup"></Route>
+          <Route path="/confirm-email"></Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
