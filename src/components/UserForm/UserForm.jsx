fa238a94631dfbe20172811fdb380b18905505ac
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-06 13:23:12 -0800

    added notes for UserForm handle submit

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 import { useForm } from '../../hooks/useForm.js';
 
+// dan also passes in an authenticate label, dunno what that is
 export default function UserForm({ onSubmit, label }) {
   const [loading, setLoading] = useState(false);
   const { formState, formError, handleFormChange, setFormError } = useForm({
     email: '',
     password: '',
   });
+
+  // there is a handle submit in the private notebook
+  // prevent default
+  // destructure email and password from formState imported above
+  // using a try/catch, it sets loading and awaits our passed in onSubmit from auth
+  //in the try there is an if check for no email and the password being less than 8 characters, throws an error if true
+  // the catch sets the form error and sets loading to false
 
   return (
     <form onSubmit={onSubmit}>
