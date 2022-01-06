500a8ea4b03ed18ed4c722d16a2501a12612418f
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-05 17:12:55 -0800

    wrote confirm email component

--- src/views/Auth/ConfirmEmail.jsx
+++ src/views/Auth/ConfirmEmail.jsx
@@ -1,7 +1,10 @@
 export default function ConfirmEmail() {
   return (
     <div>
-      <h1>Confirm Email</h1>
+      <h1>Registration Successful!</h1>
+      <p>
+        Please check your email in order to confirm your account and sign in.
+      </p>
     </div>
   );
 }
