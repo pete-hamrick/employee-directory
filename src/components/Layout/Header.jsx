58a66d8fe6444ae60222e53f568981a1f833e373
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-05 16:29:18 -0800

    added header styling

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.css';
+
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={styles.header}>
+      <Link to="/">
+        <h1>Pizza Face Employee Directory</h1>
+      </Link>
+      <p>
+        {/* TODO: when there's user context, ternary signed in as... or not signed in */}
+        Not Logged In
+        {/* button can be separate component */}
+        <button className={styles.button}>Login</button>
+      </p>
+      {/* can add breadcrumbs like dan did for the notebook */}
     </header>
   );
 }
