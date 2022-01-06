a33c55adb1a36d8920531c1c246a1fd54caa5889
Pete Hamrick <pete.hamrick@gmail.com>
2022-01-06 13:42:06 -0800

    cleaned up comments in UserForm component

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useForm } from '../../hooks/useForm.js';
 
-// dan also passes in an authenticate label, dunno what that is
 export default function UserForm({ onSubmit, label }) {
   const [loading, setLoading] = useState(false);
   const { formState, formError, handleFormChange, setFormError } = useForm({
@@ -9,12 +8,6 @@ export default function UserForm({ onSubmit, label }) {
     password: '',
   });
 
-  // there is a handle submit in the private notebook
-  // prevent default
-  // destructure email and password from formState imported above
-  // using a try/catch, it sets loading and awaits our passed in onSubmit from auth
-  //in the try there is an if check for no email and the password being less than 8 characters, throws an error if true
-  // the catch sets the form error and sets loading to false
   const handleSubmit = async (event) => {
     event.preventDefault();
     const { email, password } = formState;
@@ -22,7 +15,7 @@ export default function UserForm({ onSubmit, label }) {
     try {
       if (!email || password.length < 8) {
         setFormError(
-          'Must inclue and email and a password of 8+ characters in length'
+          'Must include and email and a password of 8+ characters in length'
         );
       }
       setLoading(true);
@@ -36,7 +29,6 @@ export default function UserForm({ onSubmit, label }) {
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
-        {/* make legend label be dynamic(pass as props from auth) */}
         <legend>{label}</legend>
 
         <label htmlFor="email">Email</label>
@@ -58,10 +50,8 @@ export default function UserForm({ onSubmit, label }) {
         />
 
         <button type="submit" disabled={loading}>
-          {/* add loading state, disable when loading and change text to authenticating */}
           {loading ? 'Verifying...' : 'Submit'}
         </button>
-        {/* add form error label here if there is a label */}
         {formError && <p>{formError}</p>}
       </fieldset>
     </form>
